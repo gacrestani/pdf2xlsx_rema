b9b487e4b86c673b7384f9b36016b6205b0fb0c5
gacrestani <gacrestani@users.noreply.github.com>
2019-07-18 14:04:26 -0300

    Create pyqt.py

---
+++ pyqt.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 18 12:22:32 2019
+
+@author: gacrestani
+"""
+
+import sys
+from PyQt5.QtWidgets import QtGui, QApplication, QLabel
+
+
+
+def window():
+    app = QtGui.QApplication(sys.argv)
+    
+    
